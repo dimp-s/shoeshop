2b847e678d6ef1bfaea3d5df6e1278387a3eda4e
Dipesh Khadgi <Dipesh0221@gmail.com>
2022-04-29 23:00:22 +0545

    created redux support on userLogin frontend

--- frontend/src/redux/Store.js
+++ frontend/src/redux/Store.js
@@ -6,20 +6,28 @@ import {
   productListReducer,
 } from './reducers/ProductReducer';
 import { cartReducer } from './reducers/CartReducer';
+import { userLoginReducer } from './reducers/UserReducer';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
+//for localstorage login
+const userInfoFromLocalStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromLocalStorage,
   },
+  userLogin: { userInfo: userInfoFromLocalStorage },
 };
 const middleware = [thunk];
 
