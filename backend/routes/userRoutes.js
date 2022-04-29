0165faa059934cba7cef196def737b70c856da8b
Dipesh Khadgi <Dipesh0221@gmail.com>
2022-04-29 18:41:10 +0545

    added register backend api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -49,4 +49,36 @@ userRouter.get(
   })
 );
 
+//register api
+userRouter.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    //to check if user exists before registering by email
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists!');
+    }
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid User Data!');
+    }
+  })
+);
+
 export default userRouter;
