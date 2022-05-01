94b2e9c2cebdf95a69c85b79fea8646ad9771ac1
Dipesh Khadgi <Dipesh0221@gmail.com>
2022-05-01 16:57:56 +0545

    created update profile redux support

--- frontend/src/components/profileComponents/ProfileTabs.js
+++ frontend/src/components/profileComponents/ProfileTabs.js
@@ -1,32 +1,96 @@
-import React from "react";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Toast from './../LoadingError/Toast';
+import Message from '../LoadingError/Error';
+import Loading from './../LoadingError/Loading';
+import { toast } from 'react-toastify';
 
 const ProfileTabs = () => {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [confirmPassword, setConfirmPassword] = React.useState('');
+  const toastId = React.useRef(null);
+
+  const toastObjects = {
+    position: 'bottom-center',
+    pauseOnFocusLoss: false,
+    draggable: false,
+    pauseOnHover: false,
+    autoClose: 2000,
+  };
+
+  const dispatch = useDispatch();
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  React.useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+    }
+  }, [dispatch, user]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //check if passwords match
+    if (password !== confirmPassword) {
+      toastId.current = toast.error("Passwords don't match !", toastObjects);
+    } else {
+      //update profile
+    }
+  };
   return (
     <>
-      <form className="row  form-container">
+      <Toast />
+      {error && <Message variant="alert-dannger">{error}</Message>}
+      {loading && <Loading />}
+      <form className="row  form-container" onSubmit={submitHandler}>
         <div className="col-md-6">
           <div className="form">
             <label for="account-fn">UserName</label>
-            <input className="form-control" type="text" required />
+            <input
+              className="form-control"
+              type="text"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
         </div>
 
         <div className="col-md-6">
           <div className="form">
             <label for="account-email">E-mail Address</label>
-            <input className="form-control" type="email" />
+            <input
+              className="form-control"
+              type="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
         </div>
         <div className="col-md-6">
           <div className="form">
             <label for="account-pass">New Password</label>
-            <input className="form-control" type="password" />
+            <input
+              className="form-control"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
         </div>
         <div className="col-md-6">
           <div className="form">
             <label for="account-confirm-pass">Confirm Password</label>
-            <input className="form-control" type="password" />
+            <input
+              className="form-control"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
           </div>
         </div>
         <button type="submit">Update Profile</button>
