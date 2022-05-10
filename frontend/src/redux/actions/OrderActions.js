2eeb96f974fcfa8cea5c5a36526185810242324f
Dipesh Khadgi <Dipesh0221@gmail.com>
2022-05-10 16:18:50 +0545

    added order details api backend and redux support

--- frontend/src/redux/actions/OrderActions.js
+++ frontend/src/redux/actions/OrderActions.js
@@ -4,6 +4,9 @@ import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
 } from '../constants/OrderConstants';
 import axios from 'axios';
 import { CART_CLEAR_ITEMS } from '../constants/CartConstants';
@@ -37,6 +40,38 @@ export const createOrder = (order) => async (dispatch, getState) => {
     }
     dispatch({
       type: ORDER_CREATE_FAIL,
+      payload: message,
+    });
+  }
+};
+
+// get order details to frontend
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_DETAILS_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/orders/${id}`, config);
+    dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === 'Not authorized, token failed!') {
+      dispatch(logout());
+    }
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
       payload: message,
     });
   }
